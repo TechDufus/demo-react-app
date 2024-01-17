a19c2a9ec395622380dc68c33cd86c3ea2aff7f7
TechDufus <matthewjdegarmo@gmail.com>
2024-01-16 23:31:24 -0600

    moar things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import ListGroup from './components/ListGroup';
 
 function App() {
-  return <div><ListGroup /></div>;
+    let items = [
+        'New York',
+        'Los Angeles',
+        'Chicago',
+        'Houston',
+        'Phoenix',
+        'Philadelphia',
+    ];
+    const handleSelectItem = (item: string) => {
+        console.log(item + ' was selected');
+    }
+
+
+    return <div>
+        <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    </div>;
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 
-function ListGroup() {
-    let items = [
-        'New York',
-        'Los Angeles',
-        'Chicago',
-        'Houston',
-        'Phoenix',
-        'Philadelphia',
-        'San Antonio',
-    ];
+// typeProps is an interface that defines the type of the props object
+interface typeProps {
+    items: string[];
+    heading: string;
+    // (item: string | number) => void is a function that takes a string or number and returns nothing
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: typeProps) {
     const [selectedIndex, setSelectedIndex] = useState(-1);
-
-
     return (
         <>
-            <h1>ListGroup</h1>
+            <h1>{heading}</h1>
             {items.length === 0 && <p>There are no items in the list.</p>}
             < ul className="list-group" >
                 {items.map((item, index) => (
@@ -26,7 +23,10 @@ function ListGroup() {
                                 : "list-group-item"
                         }
                         key={item}
-                        onClick={() => setSelectedIndex(index)}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                            onSelectItem(item);
+                        }}
                     >
                         {item}
                     </li>
